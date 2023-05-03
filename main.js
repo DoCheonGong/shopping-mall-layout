82e097496489cdb0b5b076891851f3b0045ffc76
DoCheonGong <126742693+DoCheonGong@users.noreply.github.com>
2023-05-03 17:49:35 +0900

    2023.05.03 - Overall main page design

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 $(document).ready(function(){
+    /* mouse enter/leave -> slide down/up */
     $("#gnb").on("mouseenter", function() {
-        $(".sub").stop().slideDown(); // Slide down - 조금씩 내려가기
+        $(".sub").stop().slideDown();
     });
     $("#gnb").on("mouseleave", function() {
-        $(".sub").stop().slideUp(); // Slide up - 조금씩 올라가기
+        $(".sub").stop().slideUp();
     });
-
+    /* add/remove transition effect on the main tab when mouse enter/leave */
     $("#gnb > li").on("mouseenter", function() {
-        $(this).children("a").addClass("on"); // this = #gnb > li
+        $(this).children("a").addClass("on");
     });
     $("#gnb > li").on("mouseleave", function() {
         $(this).children("a").removeClass("on");
     });
-}); // defer의 효과
+    /* button click -> hide popup */
+    $(".popup button").on("click", function() {
+        $(".popup").hide();
+    })
+    /* slider */
+    $(".content div").hide();
+    $(".tab > li > a").click(function () {
+        $(".content div").hide().filter(this.hash).fadeIn(0);
+
+        $(".tab > li").click(function () {
+            $(this).addClass("on")
+        })
+        $(".tab > li").removeClass("on")
+    }).filter(":eq(0)").click()
+});
